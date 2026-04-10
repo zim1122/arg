3df0a1358b159c119f224c6aa71f1472934ebbc7
Md.Mohtasim Billah Zim <151844024+zim1122@users.noreply.github.com>
2026-04-10 20:40:11 +0600

    Refactor line coding techniques and plotting

--- all.m
+++ all.m
@@ -1,573 +1,263 @@
-//polar nrz
-clc;
-clear;
+%% 📂 SECTION 1: LINE CODING TECHNIQUES
 
-% Input binary data
-data = [ 0 1 0 0 1 1 1 0];
+%% 1.1 Polar Non-Return-to-Zero (NRZ)
+clc; clear;
 
-% Bit and sampling specs
-bit_duration = 1;      % in seconds
-fs = 100;              % sampling frequency (Hz)
-t = 0:1/fs:bit_duration - 1/fs;  % time vector for one bit
-N = length(data);      % number of bits
+d=[0 1 0 0 1 1 1 0];
+bd=1; fs=100;
+t=0:1/fs:bd-1/fs;
+n=length(d);
+tm=0:1/fs:bd*n-1/fs;
 
-% Full signal time vector
-time = 0:1/fs:bit_duration*N - 1/fs;
-
-% Signal initialization
-polar_nrz = [];
-
-% Generate the Polar NRZ waveform
-for i = 1:N
-    if data(i) == 0
-        polar_nrz = [polar_nrz ones(1, length(t))];    % +1 V for bit 0
+s=[];
+for i=1:n
+    if d(i)==0
+        s=[s ones(1,length(t))];
     else
-        polar_nrz = [polar_nrz -ones(1, length(t))];   % -1 V for bit 1
+        s=[s -ones(1,length(t))];
     end
 end
 
-% Plotting
-figure;
-plot(time, polar_nrz, 'LineWidth', 2);
-axis([0 bit_duration*N -1.5 1.5]);
-grid on;
-xlabel('Time (s)');
-ylabel('Amplitude (V)');
-title('Polar NRZ Line Coding');
-xticks(0:bit_duration:N);
-yticks([-1 0 1]);
+figure; plot(tm,s,'LineWidth',2);
+title('Polar NRZ'); grid on;
 
-//polar-rz
 
+%% 1.2 Polar Return-to-Zero (RZ)
 clc;
-clear;
 
-% Input binary data
-data = [1 1 0 1 0 1 0 0];
+d=[1 1 0 1 0 1 0 0];
+bd=1; fs=1000;
+n=length(d);
+spb=fs*bd; h=spb/2;
+tm=0:1/fs:(n*bd)-1/fs;
 
-% Bit and sampling specs
-bit_duration = 1;     % duration of one bit in seconds
-fs = 1000;             % samples per second
-N = length(data);
-samples_per_bit = fs * bit_duration;
-half_samples = samples_per_bit / 2;
-
-% Time vector for full signal
-time = 0:1/fs:(N*bit_duration) - 1/fs;
-
-% Generate the Polar RZ waveform
-polar_rz = [];
-
-for i = 1:length(data)
-    if data(i) == 1
-        % +1 V for first half, 0 V for second half
-        bit_wave = [ones(1, half_samples), zeros(1, half_samples)];
+s=[];
+for i=1:n
+    if d(i)==1
+        w=[ones(1,h) zeros(1,h)];
     else
-        % -1 V for first half, 0 V for second half
-        bit_wave = [-ones(1, half_samples), zeros(1, half_samples)];
+        w=[-ones(1,h) zeros(1,h)];
     end
-    polar_rz = [polar_rz bit_wave];  % append the waveform for this bit
+    s=[s w];
 end
 
-% Plot the Polar RZ signal
-figure;
-plot(time, polar_rz, 'LineWidth', 2);
-axis([0 N*bit_duration -1.5 1.5]);
-grid on;
-xlabel('Time (s)');
-ylabel('Amplitude (V)');
-title('Polar RZ Line Coding');
-xticks(0:bit_duration:N);
-yticks([-1 0 1]);
+figure; plot(tm,s,'LineWidth',2);
+title('Polar RZ'); grid on;
 
-//unipolar-nrz
+
+%% 1.3 Unipolar NRZ
 clc;
-clear;
 
-% Input binary data
-data = [1 0 1 1 0 0 1 0];
+d=[1 0 1 1 0 0 1 0];
+bd=1; fs=30;
+t=0:1/fs:bd-1/fs;
+n=length(d);
+tm=0:1/fs:bd*n-1/fs;
 
-% Bit specifications
-bit_duration = 1;  % duration of one bit (in seconds)
-fs = 30;          % sampling frequency in Hz
-t = 0:1/fs:bit_duration - 1/fs;  % time vector for 1 bit
-N = length(data);  % number of bits
-
-% Time vector for entire signal
-time = 0:1/fs:bit_duration*N - 1/fs;
-
-% Signal generation
-unipolar_nrz = [];
-
-for i = 1:N
-    if data(i) == 1
-        unipolar_nrz = [unipolar_nrz ones(1, length(t))];  % High for '1'
+s=[];
+for i=1:n
+    if d(i)==1
+        s=[s ones(1,length(t))];
     else
-        unipolar_nrz = [unipolar_nrz zeros(1, length(t))]; % Low for '0'
+        s=[s zeros(1,length(t))];
     end
 end
 
-% Plotting
-figure;
-plot(time, unipolar_nrz, 'LineWidth', 2);
-axis([0 bit_duration*N 0 1]);
-grid on;
-xlabel('Time (s)');
-ylabel('Amplitude');
-title('Unipolar NRZ Line Coding');
-xticks(0:bit_duration:N);
-yticks([0 1]);
+figure; plot(tm,s,'LineWidth',2);
+title('Unipolar NRZ'); grid on;
 
-//bipolar-ami
+
+%% 1.4 Bipolar AMI
 clc;
-clear;
 
-% Input binary data
-data = [1 0 1 1 0 1 0 0 1];
+d=[1 0 1 1 0 1 0 0 1];
+bd=1; fs=100;
+spb=fs*bd;
+tm=0:1/fs:(length(d)*bd)-1/fs;
 
-% Bit and sampling specifications
-bit_duration = 1;      % duration of one bit in seconds
-fs = 100;              % sampling frequency in Hz
-samples_per_bit = fs * bit_duration;
-
-% Time vector for full signal
-time = 0:1/fs:(length(data)*bit_duration) - 1/fs;
-
-% Initialize signal
-ami = [];
-last_polarity = -1;  % Initialize to -1 so first '1' becomes +1
-
-for i = 1:length(data)
-    if data(i) == 1
-        last_polarity = -last_polarity;  % Alternate polarity
-        bit_wave = last_polarity * ones(1, samples_per_bit);
+s=[]; p=-1;
+for i=1:length(d)
+    if d(i)==1
+        p=-p;
+        w=p*ones(1,spb);
     else
-        bit_wave = zeros(1, samples_per_bit);
+        w=zeros(1,spb);
     end
-    ami = [ami bit_wave];
+    s=[s w];
 end
 
-% Plotting
-figure;
-plot(time, ami, 'LineWidth', 2);
-axis([0 length(data) * bit_duration -1.5 1.5]);
-grid on;
-xlabel('Time (s)');
-ylabel('Amplitude (V)');
-title('Bipolar AMI (Alternate Mark Inversion) Line Coding');
-xticks(0:bit_duration:length(data));
-yticks([-1 0 1]);
+figure; plot(tm,s,'LineWidth',2);
+title('Bipolar AMI'); grid on;
 
-//multilevel-2B1Q
+
+%% 1.5 2B1Q
 clc;
-clear;
 
-% Input binary data (length must be even for 2-bit grouping)
-data = [1 0 0 0 1 1 0 1];  % 8 bits = 4 symbols
+d=[1 0 0 0 1 1 0 1];
+fs=100; sd=1;
+sps=fs*sd;
+ns=length(d)/2;
+tm=0:1/fs:(ns*sd)-1/fs;
 
-% Check length
-if mod(length(data), 2) ~= 0
-    error('Input data length must be even for 2B1Q.');
+mp=containers.Map({'00','01','10','11'},[-3 -1 3 1]);
+s=[];
+
+for i=1:2:length(d)
+    pr=sprintf('%d%d',d(i),d(i+1));
+    s=[s mp(pr)*ones(1,sps)];
 end
 
-% Bit and symbol specs
-fs = 100;               % samples per second
-symbol_duration = 1;    % seconds per symbol (2 bits per symbol)
-samples_per_symbol = fs * symbol_duration;
+figure; plot(tm,s,'LineWidth',2);
+title('2B1Q'); grid on;
 
-% Time vector for full signal
-num_symbols = length(data) / 2;
-time = 0:1/fs:(num_symbols * symbol_duration) - 1/fs;
 
-% 2B1Q Mapping Table
-map = containers.Map({'00', '01', '10', '11'}, [-3, -1, 3, 1]);
-
-% Generate 2B1Q waveform
-twoB1Q = [];
-for i = 1:2:length(data)
-    pair = sprintf('%d%d', data(i), data(i+1));
-    level = map(pair);
-    twoB1Q = [twoB1Q level * ones(1, samples_per_symbol)];
-end
-
-% Plotting
-figure;
-plot(time, twoB1Q, 'LineWidth', 2);
-axis([0 num_symbols * symbol_duration -3.5 3.5]);
-grid on;
-xlabel('Time (s)');
-ylabel('Amplitude (V)');
-title('2B1Q (2 Binary 1 Quaternary) Line Coding');
-xticks(0:symbol_duration:num_symbols);
-yticks([-3 -1 0 1 3]);
-
-//bi-polarphase-manchestar
+%% 1.6 Manchester Encoding
 clc;
-clear;
 
-% Input binary data
-data = [1 0 1 1 0 0 1 0];
+d=[1 0 1 1 0 0 1 0];
+bd=1; fs=500;
+spb=fs*bd; h=spb/2;
+tm=0:1/fs:(length(d)*bd)-1/fs;
 
-% Bit and sampling parameters
-bit_duration = 1;     % seconds
-fs = 500;             % samples per second
-N = length(data);
-samples_per_bit = fs * bit_duration;
-half_samples = samples_per_bit / 2;
-
-% Time vector for full signal
-time = 0:1/fs:(N*bit_duration) - 1/fs;
-
-% Initialize Manchester waveform
-manchester = [];
-
-for i = 1:N
-    if data(i) == 0
-        % Bit 0: High to Low
-        bit_wave = [ones(1, half_samples), -ones(1, half_samples)];
+s=[];
+for i=1:length(d)
+    if d(i)==0
+        w=[ones(1,h) -ones(1,h)];
     else
-        % Bit 1: Low to High
-        bit_wave = [-ones(1, half_samples), ones(1, half_samples)];
+        w=[-ones(1,h) ones(1,h)];
     end
-    manchester = [manchester bit_wave];
+    s=[s w];
 end
 
-% Plot Manchester waveform
-figure;
-plot(time, manchester, 'LineWidth', 2);
-axis([0 N*bit_duration -1.5 1.5]);
-grid on;
-xlabel('Time (s)');
-ylabel('Amplitude (V)');
-title('Polar Biphase (Manchester) Line Coding');
-xticks(0:bit_duration:N);
-yticks([-1 0 1]);
+figure; plot(tm,s,'LineWidth',2);
+title('Manchester'); grid on;
 
-//frequency modulation
-% Parameters
-Fs = 10000;             % Sampling frequency (Hz)
-t = 0:1/Fs:0.01;        % Time vector (10 ms)
-fc = 1000;              % Carrier frequency (Hz)
-fm = 100;               % Message frequency (Hz)
-Am = 1;                 % Message amplitude
-Ac = 1;                 % Carrier amplitude
-kf = 2 * pi * 75;       % Frequency sensitivity (rad/s per unit amplitude)
 
-% Message signal
-m = Am * cos(2 * pi * fm * t);
+%% 📡 SECTION 2: ANALOG & DIGITAL MODULATION
 
-% Integrate the message signal
-int_m = cumsum(m) / Fs;
-
-% FM signal
-s_fm = Ac * cos(2 * pi * fc * t + kf * int_m);
-
-% Plotting
-figure;
-
-subplot(3,1,1);
-plot(t, m);
-title('Message Signal');
-xlabel('Time (s)');
-ylabel('Amplitude');
-
-subplot(3,1,2);
-plot(t, cos(2*pi*fc*t));
-title('Carrier Signal');
-xlabel('Time (s)');
-ylabel('Amplitude');
-
-subplot(3,1,3);
-plot(t, s_fm);
-title('FM Signal');
-xlabel('Time (s)');
-ylabel('Amplitude');
-
-//pcm
-
+%% 2.1 Amplitude Modulation (AM)
 clc;
-clear;
 
-% Parameters
-f = 5;                  % Frequency of analog signal (Hz)
-fs = 100;               % Sampling frequency (samples per second)
-t = 0:1/fs:1;           % Time vector for 1 second
-x = sin(2*pi*f*t);      % Original analog signal (sine wave)
+Fs=10000;
+t=0:1/Fs:0.01;
+fc=1000; fm=100;
+mu=0.5;
 
-% Quantization
-n_bits = 2;                      % Number of bits per sample (e.g., 3 bits ? 8 levels)
-L = 2^n_bits;                    % Number of quantization levels
-x_min = min(x); x_max = max(x);
-q_step = (x_max - x_min) / (L - 1);   % Quantization step size
+m=cos(2*pi*fm*t);
+c=cos(2*pi*fc*t);
+s=(1+mu*m).*c;
 
-% Uniform quantizer
-xq_index = round((x - x_min) / q_step);    % Get quantization index
-xq = xq_index * q_step + x_min;            % Quantized signal
-
-% Binary Encoding
-bin_pcm = dec2bin(xq_index, n_bits);       % Binary PCM values (as string matrix)
-
-% Plot original and quantized signal
 figure;
-plot(t, x, 'b', 'LineWidth', 1.5);
-hold on;
-stairs(t, xq, 'r', 'LineWidth', 1.5);
-xlabel('Time (s)');
-ylabel('Amplitude');
-title('PCM Encoding of a Sine Wave');
-legend('Original Signal', 'Quantized Signal');
-grid on;
+subplot(3,1,1); plot(t,m); title('Message');
+subplot(3,1,2); plot(t,c); title('Carrier');
+subplot(3,1,3); plot(t,s); title('AM');
 
-//delta modulation
 
+%% 2.2 Frequency Modulation (FM)
 clc;
-clear;
 
-% Parameters
-fs = 200;                    % Sampling frequency
-t = 0:1/fs:1;                % Time vector
-x = sin(2*pi*5*t);           % Input signal (sine wave)
-delta = 0.2;                 % Step size
+Fs=10000;
+t=0:1/Fs:0.01;
+fc=1000; fm=100;
+kf=2*pi*75;
 
-% Initialization
-x_dm = zeros(size(x));       % Delta modulated signal (reconstructed)
-bitstream = zeros(size(x));  % Output bitstream (0 or 1)
+m=cos(2*pi*fm*t);
+im=cumsum(m)/Fs;
+s=cos(2*pi*fc*t+kf*im);
 
-for i = 2:length(x)
-    if x(i) > x_dm(i-1)
-        bitstream(i) = 1;
-        x_dm(i) = x_dm(i-1) + delta;
+figure;
+subplot(3,1,1); plot(t,m); title('Message');
+subplot(3,1,2); plot(t,cos(2*pi*fc*t)); title('Carrier');
+subplot(3,1,3); plot(t,s); title('FM');
+
+
+%% 2.3 PCM
+clc;
+
+f=5; fs=100;
+t=0:1/fs:1;
+x=sin(2*pi*f*t);
+
+nb=2; L=2^nb;
+xmin=min(x); xmax=max(x);
+qs=(xmax-xmin)/(L-1);
+
+xi=round((x-xmin)/qs);
+xq=xi*qs+xmin;
+
+figure; plot(t,x); hold on;
+stairs(t,xq);
+title('PCM'); grid on;
+
+
+%% 2.4 Delta Modulation
+clc;
+
+fs=200;
+t=0:1/fs:1;
+x=sin(2*pi*5*t);
+dlt=0.2;
+
+xr=zeros(size(x));
+for i=2:length(x)
+    if x(i)>xr(i-1)
+        xr(i)=xr(i-1)+dlt;
     else
-        bitstream(i) = 0;
-        x_dm(i) = x_dm(i-1) - delta;
+        xr(i)=xr(i-1)-dlt;
     end
 end
 
-% Plot input and reconstructed signal
-figure;
-plot(t, x, 'b', 'LineWidth', 1.5); hold on;
-stairs(t, x_dm, 'r', 'LineWidth', 1.5);
-xlabel('Time (s)');
-ylabel('Amplitude');
-title('Delta Modulation of a Sine Wave');
-legend('Original Signal', 'Delta Modulated Reconstruction');
-grid on;
+figure; plot(t,x); hold on;
+stairs(t,xr);
+title('Delta Modulation'); grid on;
 
-//Amplitude Modulation
 
-% Parameters
-Fs = 10000;            % Sampling frequency (Hz)
-t = 0:1/Fs:0.01;       % Time vector (10 ms)
-fc = 1000;             % Carrier frequency (Hz)
-fm = 100;              % Message frequency (Hz)
-Am = 1;                % Message amplitude
-Ac = 1;                % Carrier amplitude
-mu = 0.5;              % Modulation index (0 < mu < 1 for under modulation)
+%% 〰️ SECTION 3: WAVEFORM GENERATION & ANALYSIS
 
-% Message signal
-m = Am * cos(2 * pi * fm * t);
+%% 3.1 Basic Waves
+clc;
 
-% Carrier signal
-c = Ac * cos(2 * pi * fc * t);
-
-% AM signal
-s = (1 + mu * cos(2 * pi * fm * t)) .* c;
-
-% Plotting
+t=0:0.01:10;
 figure;
 
-subplot(3,1,1);
-plot(t, m);
-title('Message Signal');
-xlabel('Time (s)');
-ylabel('Amplitude');
-
-subplot(3,1,2);
-plot(t, c);
-title('Carrier Signal');
-xlabel('Time (s)');
-ylabel('Amplitude');
-
-subplot(3,1,3);
-plot(t, s);
-title('AM Modulated Signal');
-xlabel('Time (s)');
-ylabel('Amplitude');
-
-//sqare wave
-% this program generate a square wav based on sine signals and sign()
-% function returns: +1 if the input > 0, 0 if the input = 0, and -1 if the input < 0 
-frequency = 1; % Hz
-amplitude = 2;
-duration = 10; % seconds
-samplingRate = 100; % Samples per second
-
-% Create the time vector
-t = 0:1/samplingRate:duration-1/samplingRate;
+subplot(2,2,1); plot(t,sign(sin(2*pi*t))); title('Square');
+subplot(2,2,2); plot(t,sawtooth(2*pi*t)); title('Sawtooth');
+subplot(2,2,3); plot(t,sawtooth(2*pi*t,0.5)); title('Triangular');
+subplot(2,2,4); plot(t,(square(2*pi*5*t)+1)/2); title('Pulse');
 
 
-% Create the signal
-x = amplitude * sign(sin(2 * pi * frequency * t));
-% Plot the signal
-plot(t, x,'LineWidth', 1.5);
+%% 3.2 Sinusoidal Signals
+clc;
 
-xlabel('Time (s)');
-ylabel('Amplitude');
-title(' Sine square Signal');
+t=0:0.01:4;
+y1=sin(2*pi*2*t);
+y2=cos(2*pi*2*t);
 
-//triangular_wave
-frequency = 1;       % Hz
-amplitude = 2;
-duration = 10;        % seconds
-samplingRate = 100; % Samples/second
-
-t = 0:1/samplingRate:duration;
-
-% Triangle wave from absolute value of a sawtooth-like function
-x = 4* amplitude * abs(t * frequency - floor(t * frequency + 0.5)) - amplitude;
-
-
-% Plot
-plot(t, x, 'LineWidth', 1.5);
-xlabel('Time (s)');
-ylabel('Amplitude');
-title('Manual Triangular Wave');
-
-//pulse wave
-
-f = 5;                  % Frequency (Hz)
-duty = 90;              % Duty cycle in percent
-duration = 1;           % seconds
-fs = 1000;              % Sampling rate
-t = 0:1/fs:duration;
-
-% Generate a pulse wave (10% duty cycle)
-x = square(2 * pi * f * t, duty);
-
-% Make the wave range from 0 to 1 instead of -1 to 1
-x = (x + 1) / 2;
-
-% Plot
-plot(t, x, 'LineWidth', 1.5);
-xlabel('Time (s)');
-ylabel('Amplitude');
-title('Pulse Wave (10% Duty Cycle)');
-
-//satooth wave
-
-frequency = 1;       % Hz
-amplitude = 2;
-duration = 4;        % seconds
-samplingRate = 100; % Samples per second
-
-% Time vector
-t = 0:1/samplingRate:duration-1/samplingRate;
-
-% Generate sawtooth manually (range: -A to +A)
-x = 2 * amplitude * (t * frequency - floor(t * frequency + 0.5));
-
-% Plot
-plot(t, x, 'LineWidth', 1.5);
-xlabel('Time (s)');
-ylabel('Amplitude');
-title('Manually Generated Sawtooth Signal');
-
-
-//time-cos 2pift
-
-frequency = 1; % Hz
-amplitude = 3;
-duration = 4; % seconds
-samplingRate = 30; % Samples per second
-
-% Create the time vector
-t = 0:1/samplingRate:duration;
-
-% Create the signal
-y = amplitude * cos(2 * pi * frequency * t);
-
-% Plot the signal
-plot(t, y);
-xlabel('Time (s)');
-ylabel('Amplitude');
-title('Simple Cosine Signal');
-legend ('Cosine wave');
-
-//time_cos2pift_theta.
-
-frequency = 2; % Hz
-amplitude = 3;
-duration = 4; % seconds
-samplingRate = 100; % Samples per second
-theta=pi;
-% Create the time vector
-t = 0:1/samplingRate:duration;
-
-% Create the signal
-y = amplitude * cos(2 * pi * frequency * t+theta);
-
-% Plot the signal
-plot(t, y);
-xlabel('Time (s)');
-ylabel('Amplitude');
-title('Simple cosine Signal with theta');
-
-
-//time_exponential
-
-% x(t)=A*e-alpha*t signal
-A = 5;             % Initial amplitude
-alpha = 2;             % Decay rate
-duration = 2;      % seconds
-samplingRate = 20;
-t = 0:1/samplingRate:duration;
-
-x = A * exp(-alpha * t);
-
-plot(t, x);
-xlabel('Time (s)');
-ylabel('Amplitude');
-title('Exponential Decay: x(t) = A · exp(-a·t)');
-
-//time_sin_cost2pift
-frequency = 2; % Hz
-amplitude = 3;
-duration = 4; % seconds
-samplingRate = 100; % Samples per second
-
-% Create the time vector
-t = 0:1/samplingRate:duration;
-
-% Create the signal
-y1 = amplitude * sin(2 * pi * frequency * t);
-y2 = amplitude * cos(2 * pi * frequency * t);
-% Plot the signal
-plot(t, y1);
-hold on;
+figure; plot(t,y1); hold on;
 plot(t,y2);
-xlabel('Time (s)');
-ylabel('Amplitude');
-title(' Sine cosine combined Signal');
-legend('Sine Wave', 'Cosine Wave');
-
-//frequency1
+title('Sin & Cos');
 
 
-//% Simple Frequency vs Amplitude plot
-Fs = 50;                    % Sampling frequency (Hz)
-t = 0:1/Fs:1-1/Fs;          % Time vector: 1 second
-x = sin(2*pi*50*t);           % A 50 Hz sine wave
-N = length(x);                % Signal length
-X = fft(x);                   % Compute FFT
-f = (0:N-1)*(Fs/N);           % Frequency axis
+%% 3.3 Exponential Signal
+clc;
 
-amplitude = abs(X)/N;         % Amplitude spectrum (normalized)
-disp(amplitude);
-% Plot frequency vs amplitude
-figure;
-plot(f, amplitude);
-xlabel('Frequency (Hz)');
-ylabel('Amplitude');
-title('Frequency vs Amplitude');
-%xlim([0 Fs/2]);               % Only show positive frequencies
-%grid on;
+t=0:0.05:2;
+x=5*exp(-2*t);
+
+figure; plot(t,x);
+title('Exponential');
+
+
+%% 3.4 FFT
+clc;
+
+Fs=50;
+t=0:1/Fs:1-1/Fs;
+x=sin(2*pi*5*t);
+
+N=length(x);
+X=fft(x);
+f=(0:N-1)*(Fs/N);
+
+figure; plot(f,abs(X)/N);
+title('FFT');
